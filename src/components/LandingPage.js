0745682994ac2f5e72c3fc7ed01980db2188bb73
Tim Freeman <d.timothy.freeman@gmail.com>
2017-12-10 10:52:32 -0500

    created LandingPage component

---
+++ src/components/LandingPage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class LandingPage extends React.Component {
+
+  render() {
+    return (
+      <div>
+        This is the homepage for Ed's Pick'em League.
+      </div>
+    )
+  }
+}
+
+export default LandingPage;
